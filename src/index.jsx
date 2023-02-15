65562a8ba0944d3f6f18721f013e74fed7320082
Eduardo Moreira Machado <dudumm1234@gmail.com>
2023-02-15 11:15:09 -0300

    continuação da instalação das dependências

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,8 @@
-import React from 'react';
+import { render } from 'react-dom';
 import { App } from './App';
 
+/*
+    * PRIMEIRO PARÂMETRO: o que será renderizado
+    * SEGUNDO PARÂMETRO: onde será renderizado
+*/
+render(<App />, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 // bilbioteca que unifica a forma de se declarar o caminho de um arquivo para diferentes sistemas operacionais
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// o webpack pode funcionar de formas diferentes dependendo se o ambiente for de desenvolvimento ou de produção (online)
+// variável ambiente
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
+    mode: isDevelopment ? 'development' : 'production',
+    // para visualizar o código original no console
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     // arquivo de entrada
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     // arquivo de saída
@@ -12,6 +20,14 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx']
     },
+    // devServer: {
+    //     contentBase: path.resolve(__dirname, 'public')
+    // },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
     // para tratar diferentes formatos de arquivos de difetentes formas
     module: {
         rules: [
@@ -20,6 +36,11 @@ module.exports = {
                 exclude: /node_modules/,
                 // interação do babel com o webpack
                 use: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     }
